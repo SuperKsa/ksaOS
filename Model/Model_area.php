efe16689d8937dd59498162d0831179e15f16909
cr180 <cr180@cr180.com>
2020-03-08 18:17:58 +0800

    地区扩展 增加判断函数

--- Model/Model_area.php
+++ Model/Model_area.php
@@ -16,14 +16,50 @@ if(!defined('KSAOS')) {
 }
 
 class Model_area{
-	function All(){
-		$data = DB('area')->cache('areaAll',900)->order('orders','asc')->fetch_all('id');
+    /**
+     * 读取所有地区数据
+     * @return array
+     */
+    static function All(){
+		$data = (array)DB('area')->cache('areaAll',900)->order('orders','asc')->fetch_all('id');
 		return $data;
 	}
-	
-	function data($upID=0, $select=[]){
+
+    /**
+     * 判断指定地区ID组合是否存在
+     * @param $province 省ID
+     * @param $city 市ID
+     * @param $area 县ID
+     * @param $town 镇ID
+     * @return bool
+     */
+    static function issetID($province, $city, $area, $town){
+        $data = self::All();
+        $isset = [];
+        if($province && $data[$province] && $data[$province]['upid'] ==0){
+            $isset[0] = $data[$province];
+        }
+        if($city && $data[$city] && $data[$city]['upID'] == $province){
+            $isset[1] = $data[$city];
+        }
+        if($area && $data[$area] && $data[$area]['upID'] == $city){
+            $isset[2] = $data[$area];
+        }
+        if($town && $data[$town] && $data[$town]['upID'] == $area){
+            $isset[3] = $data[$town];
+        }
+        return (!$town || $isset[3]) && (!$area || $isset[2]) && (!$city || $isset[1]) && (!$province || $isset[0]);
+    }
+
+    /**
+     * 读取指定上级ID的地区信息
+     * @param int $upID
+     * @param array $select
+     * @return array
+     */
+    static function data($upID=0, $select=[]){
 		$upID = intval($upID);
-		$query = DB('area')->cache('area_upID_'.$upID,900)->where('upID',$upID)->order('orders','asc')->fetch_all('id');
+		$query = (array)DB('area')->cache('area_upID_'.$upID,900)->where('upID',$upID)->order('orders','asc')->fetch_all('id');
 		if($select && is_array($select)){
 			$data = [];
 			foreach($query as $key => $value){
