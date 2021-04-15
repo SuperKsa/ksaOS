f92c5c619ef6055739e052a88f8fd5db4eabd23c
二营长的意大利炮 <oncwnuPbtyX85ADdk00v6nFKS3MU@git.weixin.qq.com>
2021-04-15 12:06:39 +0800

    增加小程序二维码获取函数

--- Model/Weapp.php
+++ Model/Weapp.php
@@ -78,4 +78,68 @@ class Weapp {
         }
         return false;
     }
+
+    /**
+     * 生成小程序二维码
+     * @param string $page 小程序访问路径 此参数存在时，小程序必须为正式版
+     * @param int $width 二维码宽度
+     * @param string $scene 路径参数 最大32个可见字符
+     * @param string $auto_color  自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false
+     * @param string $line_color auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
+     * @param string $is_hyaline 是否需要透明底色，为 true 时，生成透明底色的小程序
+     * @return array
+     */
+    static function getQrcode($page='', $scene='', $width=430, $auto_color=false, $line_color=['r'=>'0','g'=>'0','b'=>'0'], $is_hyaline=false){
+        $page = trim($page);
+        $auto_color = $auto_color ? true : false;
+        $is_hyaline = $is_hyaline ? true : false;
+        $width = $width > 1280 ? 1280 : $width;
+        $return = [
+            'src' => '',
+            'msg' => '生成失败'
+        ];
+        $post = [
+            'scene' => $scene,
+            'width' => (string) $width  ,
+            'auto_color' => $auto_color,
+            'line_color' => $line_color,
+            'is_hyaline' => $is_hyaline
+        ];
+        if($page){
+            $post['page'] = $page;
+        }
+        if($line_color){
+            foreach($line_color as $key => $value){
+                $line_color[$key] = (string) $value;
+            }
+            $post['line_color'] = $line_color;
+        }
+        $KEY = md5(json_encode($post));
+        $fileName = $KEY.'.'.($is_hyaline ? 'png' : 'jpg');
+        $dir = 'data/attach/weapp_qrcode/'.substr($KEY, 0, 2).'/';
+
+        if(is_file(ROOT . $dir . $fileName)){
+            $return['src'] = $dir.$fileName;
+            $return['msg'] = 'success';
+        }else {
+            $weappSetting = APP::setting('WEAPP');
+            $access_token = Wechat::AccessToken($weappSetting['APPID'], $weappSetting['AppSecret']);
+            $curl = Curls::send('https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=' . $access_token, jsonEn($post));
+            $jsonData = json_decode($curl['data'], true);
+            if ($curl['data'] && $jsonData && is_array($jsonData)) {
+                $errorLang = [
+                    45009	=> '调用分钟频率受限(目前5000次/分钟，会调整)，如需大量小程序码，建议预生成。',
+                    41030	=> '所传page页面不存在，或者小程序没有发布'
+                ];
+                $return['msg'] =$errorLang[$jsonData['errcode']] ? $errorLang[$jsonData['errcode']] : $jsonData['errmsg'];
+            } elseif ($curl['data']) {
+                Files::mkdir(ROOT . $dir);
+                if (file_put_contents(ROOT . $dir . $fileName, $curl['data']) && is_file(ROOT . $dir . $fileName)) {
+                    $return['src'] = $dir . $fileName;
+                    $return['msg'] = 'success';
+                }
+            }
+        }
+        return $return;
+    }
 }
